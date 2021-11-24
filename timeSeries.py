82dfa47769378240cd567d1204711f2f665f97af
soomin9106 <soomin9106@naver.com>
2021-11-24 18:07:39 +0900

    ARIMA time series

--- timeSeries.py
+++ timeSeries.py
@@ -25,3 +25,23 @@ model = sm.tsa.ARIMA(bitcoin_df.price.values,order=(2,1,2)) #ar =2 , differencin
 
 model_fit = model.fit()
 print(model_fit.summary())
+
+#ARIMA 모델 학습에서의 예측 결과
+forecast_data = model_fit.forecast(step=5) # 학습 데이터셋으로부터 5일 뒤의 결과 예측
+
+#test_df = 'market-price-test.csv'
+#bitcoin_test_df = pd.read_csv(test_df,names=['ds','y'])
+
+pred_y = forecast_data[0].tolist()
+pred_y_lower = []
+pred_y_upper = []
+
+#for lower_upper in forecast_data[2]:
+#    lower = lower_upper[0]
+#    upper = lower_upper[1]
+#    pred_y_lower.append(lower)
+#    pred_y_upper.append(upper)
+plt.plot(pred_y,color="gold")
+#plt.plot(pred_y_lower,color="red")
+#plt.plot(pred_y_upper,color="blue")
+plt.show()
